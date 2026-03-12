d3bb727424e25e01d3a1ff8d26c33fd669c75f70
Helmunth Ávila <helmunth@gmail.com>
2026-03-11 19:33:05 -0500

    implementar dashboard principal en el módulo web

---
+++ frontend_web/js/dashboard.js
@@ -0,0 +1,19 @@
+// Redirecciones a los módulos del sistema
+
+function irInventario(){
+
+window.location.href = "inventario.html";
+
+}
+
+function irVentas(){
+
+window.location.href = "ventas.html";
+
+}
+
+function irReportes(){
+
+window.location.href = "reportes.html";
+
+}
